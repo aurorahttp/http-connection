90a69c06919fd7e918d1818b26f165e548409470
panlatent <panlatent@gmail.com>
2018-01-21 20:47:11 +0800

    Add client connection

---
+++ src/ClientInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Aurora\Http\Connection;
+
+interface ClientInterface
+{
+    const KEEL_ALIVE = 'keep-alive';
+    const HTTP_PERSISTENT = '1.1';
+    const HTTP_CLOSE = 'close';
+    const HTTP_1_0 = '1.0';
+    const HTTP_1_1 = '1.1';
+    const HTTP_2_0 = '2.0';
+
+    public function read($length);
+
+    public function write($content, $length = 0);
+
+    public function send($content);
+
+    public function getSocket();
+}
