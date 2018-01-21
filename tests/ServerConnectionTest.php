b9dcce68b9e8baf45b3c5cc40ac392a87b129627
panlatent <panlatent@gmail.com>
2018-01-22 00:02:35 +0800

    Fix unit test socket port

--- tests/ServerConnectionTest.php
+++ tests/ServerConnectionTest.php
@@ -7,39 +7,46 @@ class ServerConnectionTest extends TestCase
 {
     public function test__construct()
     {
-        $server = new ServerConnection('127.0.0.1', 10000);
+        $server = new ServerConnection('127.0.0.1', $this->getPort());
         $this->assertAttributeEquals('127.0.0.1', 'address', $server);
         $this->assertAttributeNotEmpty('createdAt', $server);
     }
 
     public function testClose()
     {
-        $server = new ServerConnection('127.0.0.1', 10000);
+        $server = new ServerConnection('127.0.0.1', $this->getPort());
         $server->close();
         $this->assertAttributeNotEmpty('closedAt', $server);
     }
 
     public function testGetSocket()
     {
-        $server = new ServerConnection('127.0.0.1', 10000);
+        $server = new ServerConnection('127.0.0.1', $this->getPort());
         $this->assertInternalType('resource', $server->getSocket());
     }
 
     public function testGetAddress()
     {
-        $server = new ServerConnection('127.0.0.1', 10000);
+        $server = new ServerConnection('127.0.0.1', $this->getPort());
         $this->assertEquals('127.0.0.1', $server->getAddress());
 
         $server = new ServerConnection('*', 10000);
         $this->assertEquals('0.0.0.0', $server->getAddress());
 
-        $server = new ServerConnection('localhost', 10000);
+        $server = new ServerConnection('localhost', $this->getPort());
         $this->assertEquals('127.0.0.1', $server->getAddress());
     }
 
     public function testGetPort()
     {
-        $server = new ServerConnection('127.0.0.1', 10000);
+        $server = new ServerConnection('127.0.0.1', $this->getPort());
         $this->assertEquals(10000, $server->getPort());
+    }
+
+    private function getPort()
+    {
+        static $port = 19900;
+
+        return $port++;
     }
 }
